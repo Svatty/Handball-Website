314a564d116c5d27ce9851190ff773e83fc59a16
Svatty <108821684+Svatty@users.noreply.github.com>
2024-12-15 23:46:08 +0100

    Abgabeprojekt
    Mein Abgabeprojekt.
    Ich habe auf einem 1980x1080 Bildschrim die Website entwickelt und ich kann jetzt schon sagen das es schlecht aussehen wird, wenn die Resolution nicht passt. Ich hoffe trotzdem das es passen wird.

--- HTML_Code/script.js
+++ HTML_Code/script.js
@@ -1,4 +1,17 @@
+const sidenav = document.getElementById('side_navbar')
+const overlay = document.getElementById('overlay');
+const body = document.getElementsByTagName('body')[0];
+
 function open_sidenav(){
-	const img = document.getElementById('side_navbar');
-	img.style.left = `${img.offsetLeft + 200px}px`;
+  sidenav.style.display ='block';
+  overlay.style.display ='block';
+  body.style.overflowY ='hidden';
 }
+
+function close_sidenav(){
+  sidenav.style.display ='none';
+  overlay.style.display ='none';
+  body.style.overflowY = 'auto';
+}
+
+document.getElementById('overlay').addEventListener('click', close_sidenav);
